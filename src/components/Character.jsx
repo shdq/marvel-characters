5ec4ab40987485f7ed83b336b0ecbbcf25ead23e
Sergei Cherniaev <sergei@cherniaev.com>
2019-08-07 10:36:58 +0300

    switched to function component and refactored

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import './Character.css';
+import "./Character.css";
 
-import Image from './Image';
+import Image from "./Image";
 
-class Character extends React.Component {
-  render() {
-    const detailsUrl = this.props.urls.map((link, i) => { 
-      if(link.type === 'detail')
-        return <li className="character__urls-list-item" key={i}><a className="link" href={link.url} rel="noopener" target="_blank">{this.props.name} on marvel.com</a></li>
-    })
-    return (
-      <div className="character">
-        <Image imageClass="character__image" src={this.props.image} alt={this.props.name} />
-        <h2 className="character__name">{this.props.name}</h2>
-        <p className="character__description">{this.props.desc}</p>
-        <ul className="character__urls-list">{detailsUrl}</ul>
-      </div>
-    );
-  }
-}
+const Character = props => {
+  const detailsUrl = props.urls
+    .filter(link => link.type === "detail")
+    .map((link, i) => {
+      return (
+        <li className="character__urls-list-item" key={i}>
+          <a
+            className="link"
+            href={link.url}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {props.name} on marvel.com
+          </a>
+        </li>
+      );
+    });
+  return (
+    <div className="character">
+      <Image imageClass="character__image" src={props.image} alt={props.name} />
+      <h2 className="character__name">{props.name}</h2>
+      <p className="character__description">{props.desc}</p>
+      <ul className="character__urls-list">{detailsUrl}</ul>
+    </div>
+  );
+};
 
 export default Character;
