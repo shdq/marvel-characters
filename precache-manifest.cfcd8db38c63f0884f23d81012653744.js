bf39bee0ea7013aaaf28c308b6493533c6c165d6
Sergei Cherniaev <sergei@cherniaev.com>
2019-08-07 10:58:27 +0300

    Updates

---
+++ precache-manifest.cfcd8db38c63f0884f23d81012653744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cd84eafc6aef29cef5ecc44cf53975",
+    "url": "/marvel-characters/index.html"
+  },
+  {
+    "revision": "1df78987ede82abb8989",
+    "url": "/marvel-characters/static/css/2.c048c4a8.chunk.css"
+  },
+  {
+    "revision": "fdb5654bed1a472fcf05",
+    "url": "/marvel-characters/static/css/main.e10dc65b.chunk.css"
+  },
+  {
+    "revision": "1df78987ede82abb8989",
+    "url": "/marvel-characters/static/js/2.23021e17.chunk.js"
+  },
+  {
+    "revision": "fdb5654bed1a472fcf05",
+    "url": "/marvel-characters/static/js/main.2a908086.chunk.js"
+  },
+  {
+    "revision": "e46c73d2a3e37d4a2e0e",
+    "url": "/marvel-characters/static/js/runtime~main.5c5e3208.js"
+  }
+]);
